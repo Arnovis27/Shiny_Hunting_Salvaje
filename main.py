b3847792d1286d9fac086fd5e73235ea8155c5f8
Arnovis27 <arnovisdee@hotmail.com>
2023-12-17 14:33:10 -0500

    ultimando detalles de ciclo repetitivo

--- main.py
+++ main.py
@@ -10,22 +10,32 @@ def main():
     valor, dimension= ventana()
 
     if valor == True:
-        buscar_pokemon()
-        time.sleep(1) #Segundos de aparicion del pokemon salvaje 
-        presionar_b()
-        time.sleep(1)
-        captura_pantalla(dimension)
-        entorno= cv2.imread("C:/Users/arnov/Documents/Python/Shiny_Salvaje/Captura/captura_escene.png")
-        
-        #se verifica si entro en combate o no
-        combate_presente= metodo_emparejamiento(entorno, combate, umbral)
-        if combate_presente:
-            for i in range(3): #Ajustar al numero de pokemon que aparece  por rutas
-                sprite= cv2.imread("C:/Users/arnov/Documents/Python/Shiny_Salvaje/Ruta/Desierto/{}.PNG".format(i+1))
-                pokemon_presente= metodo_emparejamiento(entorno, sprite, umbral)
+        contador_encuentros=0
 
-                if pokemon_presente:
-                    huir()
+        while True:
+            aux=0 #se vuelve 1 si al menos el pokemon fue identificado una vez dentro la busqueda
+            buscar_pokemon()
+            time.sleep(1) #Segundos de aparicion del pokemon salvaje 
+            presionar_b()
+            time.sleep(1)
+            captura_pantalla(dimension)
+            entorno= cv2.imread("C:/Users/arnov/Documents/Python/Shiny_Salvaje/Captura/captura_escene.png")
+            
+            #se verifica si entro en combate o no
+            combate_presente= metodo_emparejamiento(entorno, combate, umbral)
+            if combate_presente:
+                for i in range(3): #Ajustar al numero de pokemon que aparece  por rutas
+                    sprite= cv2.imread("C:/Users/arnov/Documents/Python/Shiny_Salvaje/Ruta/Desierto/{}.PNG".format(i+1))
+                    pokemon_presente= metodo_emparejamiento(entorno, sprite, umbral)
+
+                    if pokemon_presente:
+                        huir()
+                        aux= 1
+
+                if aux==0:
+                    break #rompe el ciclo, while, porque ha encontrado shiny?
+
+                print("Pokemon salvaje #{}".format(contador_encuentros+1))
                 
 
 if __name__ == '__main__':
